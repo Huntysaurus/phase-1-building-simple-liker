b8bb12c790f873877002921c24537b7e65946546
Jacob <yawjacob@yahoo.com>
2021-10-30 15:38:35 -0500

    Done.

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const buttons = document.querySelectorAll('span.like-glyph')
+console.log(buttons)
 
+buttons.forEach(buttons => buttons.addEventListener('click', clickLike))
 
+function clickLike(buttons){
+  console.log(buttons.target)
+  mimicServerCall()
+  .then(() => {
+    if (buttons.target.innerTEXT === EMPTY_HEART) {
+      buttons.target.innerTEXT = FULL_HEART
+    }
+    else if (buttons.target.innerTEXT === FULL_HEART) {
+      buttons.target.innerTEXT = EMPTY_HEART
+    }
+  })
+  .catch(() => {
+    const erMsg = document.getElementById('modal')
+    console.log(erMsg)
+    erMsg.className = 'show'
 
+  setTimeout(() => {
+    const erMsg = document.getElementById("modal")
+    console.log(erMsg)
+    erMsg.className = "hidden"}, 3000)}
+  )}
+  })
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
